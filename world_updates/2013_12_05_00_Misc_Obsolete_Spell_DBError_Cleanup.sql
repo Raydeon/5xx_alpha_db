8d1ae50c8f7fa3fa42e2426d45c5d2c2995d2821
Raydeon <stengray@hotmail.com>
2013-12-06 07:31:32 -0700

    Update git ignore list. Added current world updates (through build f1ffbdc06ec67d90dce2b95fac26694effcb09c1). Also added minor db fixes clearing some console errors for outdated spells.

---
+++ world_updates/2013_12_05_00_Misc_Obsolete_Spell_DBError_Cleanup.sql
@@ -0,0 +1,42 @@
+/*CREDITS:
+Raydeon
+*/
+
+-- Update outdated spells for Demolisher -- Info pulled from http://www.wowhead.com/npc=34775
+UPDATE creature_template SET spell1=67441, spell2=67440 WHERE entry=34775;
+-- Update outdated spells for Siege Engine -- Info pulled from http://www.wowhead.com/npc=34776
+UPDATE creature_template SET spell1=67796, spell2=67797 WHERE entry=34776;
+-- Update outdated spells for Glaive Thrower -- Info pulled from http://www.wowhead.com/npc=35273
+UPDATE creature_template SET spell1=67034, spell2=67195 WHERE entry=35273;
+-- Update outdated spells for Siege Engine -- Info pulled from http://www.wowhead.com/npc=35069
+UPDATE creature_template SET spell1=67796, spell2=67797 WHERE entry=35069;
+-- Update outdated spells for Glaive Thrower -- Info pulled from http://www.wowhead.com/npc=34802
+UPDATE creature_template SET spell1=67034, spell2=67195 WHERE entry=34802;
+-- Update outdated spells for Defias Blackguard -- NPC NO LONGER IN GAME -- Info pulled from http://www.wowhead.com/npc=636
+UPDATE creature_template SET spell1=0, spell2=0 WHERE entry=636;
+-- Update outdated spells for Dragonmaw Centurion -- Info pulled from http://www.wowhead.com/npc=1036
+UPDATE creature_template SET spell1=78660, spell2=76622, spell3=3419, spell4=0 WHERE entry=1036;
+-- Update outdated spells for Dragonmaw Bonewarder -- Info pulled from http://www.wowhead.com/npc=1057
+UPDATE creature_template SET spell1=77721, spell2=78646, spell3=78647, spell4=0 WHERE entry=1057;
+-- Update outdated spells for Frostmane Snowstrider -- NPC NO LONGER IN GAME -- Info pulled from http://www.wowhead.com/npc=1121
+UPDATE creature_template SET spell1=0, spell2=0 WHERE entry=1121;
+-- Update outdated spells for Defias Inmate -- NPC NO LONGER IN GAME -- Info pulled from http://www.wowhead.com/npc=1708
+UPDATE creature_template SET spell1=0, spell2=0, spell3=0 WHERE entry=1708;
+-- Update outdated spells for Highlord Taelan Fordring <Highlord of the Scarlet Crusade> -- NPC NO LONGER IN GAME -- Info pulled from http://www.wowhead.com/npc=1842
+UPDATE creature_template SET spell1=0, spell2=0, spell3=0, spell4=0 WHERE entry=1842;
+-- Update outdated spells for Thule Ravenclaw -- NPC NEEDS SCRIPTED!!! -- Info pulled from http://www.wowhead.com/npc=1947
+UPDATE creature_template SET spell1=0, spell2=0, spell3=0, spell4=0 WHERE entry=1947;
+-- Update outdated spells for Foreman Bonds -- NPC NO LONGER IN GAME -- Info pulled from http://www.wowhead.com/npc=2305
+UPDATE creature_template SET spell1=0, spell2=0, spell3=0, spell4=0 WHERE entry=2305;
+-- Update outdated spells for Witherbark Shadow Hunter -- NO LONGER CURSES -- Info pulled from http://www.wowhead.com/npc=2557
+UPDATE creature_template SET spell3=0 WHERE entry=2557;
+-- Update outdated spells for Shadowforge Commander -- NO LONGER CASTS AURA -- Info pulled from http://www.wowhead.com/npc=2744
+UPDATE creature_template SET spell1=0 WHERE entry=2744;
+-- Update outdated spells for Bristleback Thornweaver -- Info pulled from http://www.wowhead.com/npc=3261
+UPDATE creature_template SET spell2=9739 WHERE entry=3261;
+-- Update outdated spells for Razormane Thornweaver -- Info pulled from http://www.wowhead.com/npc=3268
+UPDATE creature_template SET spell1=21335 WHERE entry=3268;
+-- Update outdated spells for Mana Spring Totem -- NPC NO LONGER IN GAME -- Info pulled from http://www.wowhead.com/npc=3573
+UPDATE creature_template SET spell1=0, spell2=0, spell3=0, spell4=0 WHERE entry=3573;
+-- Update outdated spells for Stoneclaw Totem -- NPC NO LONGER IN GAME -- Info pulled from http://www.wowhead.com/npc=3579
+UPDATE creature_template SET spell1=0, spell2=0, spell3=0, spell4=0 WHERE entry=3579;
